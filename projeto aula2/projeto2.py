c7f1e85ea646b50af9b5204b39d82d4b1436ddfe
LucOliverfn <lucas.ofn@gmail.com>
2023-05-30 22:05:45 -0300

    h

--- projeto aula2/projeto2.py
+++ projeto aula2/projeto2.py
@@ -1,15 +1,11 @@
 import pyautogui
 import pyperclip
 from time import sleep
-# pyautogui.hotkey("ctrl", "t")
-# sleep(2)
-# pyperclip.copy("www.gmail.com")
-# sleep(2)
-# pyautogui.hotkey("ctrl", "v")
-# sleep(2)
-# pyautogui.hotkey("enter")
+pyautogui.hotkey("ctrl", "t")
+sleep(2)
+pyperclip.copy("www.gmail.com")
+sleep(2)
+pyautogui.hotkey("ctrl", "v")
+sleep(2)
+pyautogui.hotkey("enter")
 
-sleep(5)
-posicão = pyautogui.position()
-
-print (posicão)
